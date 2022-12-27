cb2eebe771dcc3a1044df4a9cbfeca99590575ac
somnathtelang <121513213+somnathtelang@users.noreply.github.com>
2022-12-28 01:07:05 +0530

    first commit

---
+++ File1.java
@@ -0,0 +1,7 @@
+public class File1
+{
+public static void main(String[] args)
+{
+System.out.println("hellow World ");
+}
+}
---
+++ File2.java
@@ -0,0 +1,6 @@
+public class File2
+{
+public static void main(String [] args)
+{
+int x= 10;
+System.out.println(x);
